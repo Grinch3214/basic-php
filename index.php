ac22ca80ef8b103523968782f767ae422a1d5f6a
Max <scorpiomax90@gmail.com>
2024-03-24 14:18:15 +0200

    use strict types

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-	
+
+declare(strict_types=1);
+
+$paymentStatus = 2;
+
+function getStatus(int|float $pay, bool $showMessage = false) : ?string {
+	$message = match($pay) {
+		1 => 'Success',
+		2 => 'Error',
+		default => 'Unknown'
+	};
+
+	if ($showMessage) {
+		return null;
+	}
+
+	return $message;
+}
+
+$statusMessage = getStatus($paymentStatus);
+var_dump($statusMessage);
+
+
+echo '<br>';
+
 ?>
