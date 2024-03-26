7181cd19e32bff36031d6d8ee546c7e7fa480b70
Max <scorpiomax90@gmail.com>
2024-03-26 20:59:33 +0200

    if/else

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-$number = 45.455;
-echo round($number, 2, PHP_ROUND_HALF_DOWN);
+
+// admin = 1, mod = 2, any other numbers
+
+$permission = 3;
+
+if ($permission === 1) {
+	?><h1>Hello, Admin!</h1><?php
+} else if ($permission === 2) {
+	?><h1>Hello, mod</h1><?php
+} else {
+	?><h1>Hello, user</h1><?php
+}
