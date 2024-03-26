e73d45cbddafbad0083eb8fa3298879ed92fa4b3
Max <scorpiomax90@gmail.com>
2024-03-26 09:15:54 +0200

    Resistor Colors

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+declare(strict_types=1);
+
+$colors = [
+	"black" => 0,
+	"brown" => 1,
+	"red" => 2,
+	"orange" => 3,
+	"yellow" => 4,
+	"green" => 5,
+	"blue" => 6,
+	"violet" => 7,
+	"grey" => 8,
+	"white" => 9
+];
+
+function colorCode($colors ,$color) {
+	return $colors[$color];
+}
+
+echo colorCode($colors, 'green');
