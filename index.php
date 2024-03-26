5ff9f30e4ad5d6cc4761ea964e35e71f160d11d7
Max <scorpiomax90@gmail.com>
2024-03-26 19:23:55 +0200

    unsetting variables

--- index.php
+++ index.php
@@ -1,41 +1,10 @@
 <?php
-declare(strict_types=1);
 
-$colors = [
-	"black" => 0,
-	"brown" => 1,
-	"red" => 2,
-	"orange" => 3,
-	"yellow" => 4,
-	"green" => 5,
-	"blue" => 6,
-	"violet" => 7,
-	"grey" => 8,
-	"white" => 9
-];
+$names = ['John', 'Jane', 'Bob'];
+print_r($names);
 
-function colorCode(array $colors ,string $color): int {
-	return $colors[$color];
-}
+unset($names[1]);
 
-echo colorCode($colors, 'green');
-echo "<br>";
+$names = array_values($names);
 
-function twoFer(string $name = "you"): string {
-	return "One for {$name}, one for me.";
-}
-
-echo twoFer('John');
-echo "<br>";
-
-function isLeap(int $year): bool {
-	if($year % 4 !== 0) {
-		return false;
-	}
-	if($year % 100 === 0 && $year % 400 !== 0) {
-		return false;
-	}
-	return true;
-}
-
-var_dump(isLeap(2024));
+print_r($names);
