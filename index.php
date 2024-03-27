3dfcd7e4b978b8fcffec9d49b250ef599fccb863
Max <scorpiomax90@gmail.com>
2024-03-27 11:06:37 +0200

    named arguments

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php 
 
-function sum(bool $dumpArr, int|float ...$nums) {
-	if($dumpArr) {
-		var_dump($nums);
-		return;
-	}
-	return array_sum($nums);
+function sum(int|float $a, int|float $b) {
+	var_dump($a, $b);
+	return $a + $b;
 }
 
-echo sum(false, 5, 2, 3, 8);
-echo '<br>';
-echo sum(true, 5, 2, 3, 8);
+echo sum(b:2, a:5);
