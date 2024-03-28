51ccea9bb2e03ddfeb795aaa09c81e77f4893bde
Max <scorpiomax90@gmail.com>
2024-03-29 00:00:15 +0200

    randomizer robot name

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php 
+
+function getNewName():string {
+	$arrayOfLetters = range('A', 'Z');
+	shuffle($arrayOfLetters);
+	$num = mt_rand(100, 999);
+
+	return "{$arrayOfLetters[0]}{$arrayOfLetters[1]}{$num}";
+}
+
+echo "Robot name: " . getNewName() . "<br>";
