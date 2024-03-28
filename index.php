8c1d8cb09d57b037c57faa74e7068646d252227c
Max <scorpiomax90@gmail.com>
2024-03-28 23:43:18 +0200

    clear basic branch

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
 <?php 
-
-if(file_exists('example.txt')) {
-	echo filesize('example.txt');
-
-	file_put_contents('example.txt', 'Hello test!');
-
-	clearstatcache();
-
-	echo file_get_contents('example.txt');
-}
