ac74666cf641ea8ed8d0401fb7c345c5271e44a5
Max <scorpiomax90@gmail.com>
2024-03-30 13:38:43 +0200

    OOP php #2

--- Account.php
+++ Account.php
@@ -1,11 +1,10 @@
 <?php
 
 class Account {
-	public string $name;
-	public float|int $balance;
 
-	public function __construct(string $newName, float|int $newBalance) {
-		$this->name = $newName;
-		$this->balance = $newBalance;
+	public function __construct(
+			public string $name,
+			public float|int $balance
+		) {
 	}
 }
